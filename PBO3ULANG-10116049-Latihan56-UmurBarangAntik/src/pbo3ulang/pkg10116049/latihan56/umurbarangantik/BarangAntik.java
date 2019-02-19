dbc849d60e662d597e0d342d64c8aae804a24b3c
M Ibad Guthwara <43487906+ibadguthwara@users.noreply.github.com>
2019-02-19 20:55:13 +0700

    Add files via upload

---
+++ PBO3ULANG-10116049-Latihan56-UmurBarangAntik/src/pbo3ulang/pkg10116049/latihan56/umurbarangantik/BarangAntik.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3ulang.pkg10116049.latihan56.umurbarangantik;
+
+/**
+ *
+ * @author ibadguthwara
+ */
+public class BarangAntik {
+    int umur;
+    
+    public BarangAntik(int umur){
+    this.umur=umur;
+    }
+    public void tampilUmur(){
+        System.out.println("Umur Barang Antik ini adalah : "+umur+" tahun");
+    }
+    
+}
---
+++ PBO3ULANG-10116049-Latihan56-UmurBarangAntik/src/pbo3ulang/pkg10116049/latihan56/umurbarangantik/Radio.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3ulang.pkg10116049.latihan56.umurbarangantik;
+
+/**
+ *
+ * @author ibadguthwara
+ */
+public class Radio extends BarangAntik{
+     private String name;
+
+    public Radio(int umur) {
+        super(umur);
+        this.umur=umur;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+}
